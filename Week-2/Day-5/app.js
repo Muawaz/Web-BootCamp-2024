68ff09bdf6477ee9b99f6fe9da7b8807e54c540c
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-13 16:10:29 +0500

    running google OAuth

--- Week-2/Day-5/app.js
+++ Week-2/Day-5/app.js
@@ -75,14 +75,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    res.send('Sign in with Google');
-});
-
-app.get('/google/callback', async (req, res) => {
-    res.send("Google OAuth Callback Url !");
-});
-
 const port = process.env.PORT || 3000;
 
 const start = async (port) => {
@@ -95,15 +87,15 @@ start(port);
 
 const GOOGLE_OAUTH_URL = process.env.GOOGLE_OAUTH_URL;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CALLBACK_URL = "http%3A//localhost:8000/google/callback";
+const GOOGLE_CALLBACK_URL = "http://localhost:8000/google/callback";
 const GOOGLE_OAUTH_SCOPES = [
-    "https%3A//www.googleapis.com/auth/userinfo.email",
-    "https%3A//www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
 ]
 
 app.get('/', async (req, res) => {
     const state = 'some_state';
-    const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
+    const scopes = GOOGLE_OAUTH_SCOPES.join(' ');
     const GOOGLE_OAUTH_CONSENT_URL = `${GOOGLE_OAUTH_URL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}&access_type=offline&response_type=code&state=${state}&scope=${scopes}`;
     res.redirect(GOOGLE_OAUTH_CONSENT_URL);
 });
@@ -124,30 +116,44 @@ app.get('/google/callback', async (req, res) => {
     };
     console.log(data);
 
-    //exchange authorization code for acess token & id_token
-    const response = await fetch(GOOGLE_ACCESS_TOKEN_URL, {
-        method: 'POST',
-        body: JSON.stringify(data),
-    });
-    const access_token_data = await response.json();
+    try {
+        //exchange authorization code for acess token & id_token
+        const response = await fetch(GOOGLE_ACCESS_TOKEN_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams(data),
+        });
+        const access_token_data = await response.json();
+    
+        const { id_token } = access_token_data;
+        console.log(id_token);
+    
+        // verify and extract the information in the id token
+    
+        const token_info_resposne = await fetch ( 
+            `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
+        );
+        const token_info_data = await token_info_resposne.json();
+        res.status(token_info_resposne.status).json( token_info_data);
+    
+        
+        const { email, name } = token_info_data;
 
-    const { id_token } = access_token_data;
-    console.log(id_token);
-
-    // verify and extract the information in the id token
-
-    const token_info_resposne = await fetch ( 
-        `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
-    );
-    res.status(token_info_resposne.status).json( await token_info_resposne.json());
-
-    const { email, name } = token_info_data;
-    let user = await User.findOne({ email }).select('-password');
-    if(!user) {
-        user = await User.create({ email, name});
+        let user = await User.findOne({ email }).select('-password');
+        if(!user) {
+            user = await User.create({ email, name});
+        }
+        const token = user.generateToken();
+        // res.status(token_info_resposne.status).json({ user, token });
+        res.status(200).json({ user, token });
     }
-    const token = user.generateToken();
-    res.status(token_info_resposne.status).json({ user, token });
+    catch (error) {
+        console.error('Error during OAuth callback', error);
+        if (!res.headersSent) {
+            res.status(500).json({ error: 'Internal Server error' });
+        }
+    }
+
 
 });
 
