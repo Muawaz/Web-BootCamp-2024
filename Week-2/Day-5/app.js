229679cda7c7d50a7b574c9b003aaddcdb721ab7
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-13 12:35:11 +0500

    before start using mongoos and JWT

--- Week-2/Day-5/app.js
+++ Week-2/Day-5/app.js
@@ -41,3 +41,57 @@ const start = async (port) => {
 };
 
 start(port);
+
+const GOOGLE_OAUTH_URL = process.env.GOOGLE_OAUTH_URL;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CALLBACK_URL = "http%3A//localhost:8000/google/callback";
+
+const GOOGLE_OAUTH_SCOPES = [
+    "https%3A//www.googleapis.com/auth/userinfo.email",
+    "https%3A//www.googleapis.com/auth/userinfo.profile",
+]
+
+app.get('/', async (req, res) => {
+    const state = 'some_state';
+    const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
+    const GOOGLE_OAUTH_CONSENT_URL = `${GOOGLE_OAUTH_URL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}&access_type=offline&response_type=code&state=${state}&scope=${scopes}`;
+    res.redirect(GOOGLE_OAUTH_CONSENT_URL);
+});
+
+
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_ACCESS_TOKEN_URL = process.env.GOOGLE_ACCESS_TOKEN_URL;
+
+app.get('/google/callback', async (req, res) => {
+    console.log(req.query);
+    const { code } = req.query;
+    const data = {
+        code,
+        client_id: GOOGLE_CLIENT_ID,
+        client_secret: GOOGLE_CLIENT_SECRET,
+        redirect_uri: 'http://localhost:8000/google/callback',
+        grant_type: 'authorization_code',
+    };
+    console.log(data);
+
+    //exchange authorization code for acess token & id_token
+    const response = await fetch(GOOGLE_ACCESS_TOKEN_URL, {
+        method: 'POST',
+        body: JSON.stringify(data),
+    });
+    const access_token_data = await response.json();
+
+    const { id_token } = access_token_data;
+    console.log(id_token);
+
+    // verify and extract the information in the id token
+
+    const token_info_resposne = await fetch ( 
+        `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
+    );
+    res.status(token_info_resposne.status).json( await token_info_resposne.json());
+    
+    
+
+});
+
