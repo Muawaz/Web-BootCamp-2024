73c04995b5f3c6cedee2ac2666ca2e9457e4f7ae
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-13 15:21:21 +0500

    basic authz app

--- Week-2/Day-5/app.js
+++ Week-2/Day-5/app.js
@@ -16,10 +16,61 @@
 //     console.log('Server started on port 3000');
 // })
 
-import * as dotenv from 'dotenv'
-dotenv.config();
-import express from 'express';
-import fetch from 'node-fetch';
+// import * as dotenv from 'dotenv'
+// dotenv.config();
+// import express from 'express';
+// import fetch from 'node-fetch';
+// import mongoose from 'mongoose';
+// import jwt from 'jsonwebtoken';
+
+
+require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
+
+
+// =============================================DATABASE
+const mongoDBURI = process.env.MONGO_DB_URI;
+mongoose.connect(mongoDBURI)
+.then(() => console.log('MongoDB connected Successfully'))
+.catch(err => console.log("MongoDB conenction error", err));
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: [true, 'Please provide a name'],
+        minlength: 3,
+        maxlength: 56,
+    },
+    email: {
+        type: String,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: false,
+    },
+});
+
+UserSchema.methods.generateToken = function () {
+    const token = jwt.sign({ id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME,
+    });
+    return token;
+};
+
+const User = mongoose.model('User', UserSchema);
+
+// =====================================================================
 const app = express();
 
 app.use(express.json());
@@ -45,7 +96,6 @@ start(port);
 const GOOGLE_OAUTH_URL = process.env.GOOGLE_OAUTH_URL;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CALLBACK_URL = "http%3A//localhost:8000/google/callback";
-
 const GOOGLE_OAUTH_SCOPES = [
     "https%3A//www.googleapis.com/auth/userinfo.email",
     "https%3A//www.googleapis.com/auth/userinfo.profile",
@@ -90,8 +140,14 @@ app.get('/google/callback', async (req, res) => {
         `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
     );
     res.status(token_info_resposne.status).json( await token_info_resposne.json());
-    
-    
+
+    const { email, name } = token_info_data;
+    let user = await User.findOne({ email }).select('-password');
+    if(!user) {
+        user = await User.create({ email, name});
+    }
+    const token = user.generateToken();
+    res.status(token_info_resposne.status).json({ user, token });
 
 });
 
