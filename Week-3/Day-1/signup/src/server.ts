f01de2a5a50b42a5c1cd5928bffa863808c8bbdb
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-16 17:22:09 +0500

    Back to one file solution

--- Week-3/Day-1/signup/src/server.ts
+++ Week-3/Day-1/signup/src/server.ts
@@ -1,6 +1,7 @@
-import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
-import pgPromise from 'pg-promise';
+import express, {Request, Response} from 'express'
+import { Client } from 'pg'
+import pgPromise from 'pg-promise'
+import dotenv from 'dotenv'
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,7 +18,60 @@ const dbName = process.env.DB_NAME;
 const dbConnectionString = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`
 const db = pgp(dbConnectionString)
 
+const client = new Client({
+    user: dbUser,
+    host: dbHost,
+    password: dbPassword,
+    port: dbPort,
+});
+
+const setupDatabase = async () => {
+    try{
+        await client.connect()
+        console.log('Connected to PostgresSQL');
+
+        await client.query(`CREATE DATABASE ${dbName}`);
+        console.log('Database Created');
+
+        await client.end();
+
+        const db = pgp(dbConnectionString);
+
+        await db.none(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+            )
+        `);
+        console.log('Table Created')
+    } catch( error) {
+        console.error('Error during database setup : ', error);
+    }
+};
+
+setupDatabase();
+
 app.use(express.json());
+
+// create table
+app.post('/create-table', async (req: Request, res: Response) => {
+    try{
+        await db.none(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+            )
+        `);
+        res.status(200).send('Table created');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error creating table');
+    }
+})
 
 // add user
 app.post('/add-user', async (req: Request, res: Response) => {
